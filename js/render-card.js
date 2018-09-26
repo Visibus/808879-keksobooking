48bd3cc60fb5acce17b94ecc570d88644f9fab0b
Visibus <visibus@mail.ru>
2018-09-26 16:44:26 +0300

    Личный проект: надо подкачаться

--- js/render-card.js
+++ js/render-card.js
@@ -32,8 +32,7 @@
     var childElement = cardElement.querySelector('.popup__features').querySelectorAll('li');
     for (var i = 0; i < data.aFeatures.length; i++) {
     // Удаляемый элемент
-      var findInd = objCard.offer.features.indexOf(data.aFeatures[i]);
-      if (findInd === -1) {
+      if (!~objCard.offer.features.indexOf(data.aFeatures[i])) {
         cardElement.querySelector('.popup__features').removeChild(childElement[i]);
       }
     }
